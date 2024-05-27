6623cef8fe1b130fd9ab73fb727c14fbaf4f7aa7
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2024-05-27 11:27:46 -0500

    2.0.1 Addition: Chat components created, without logic

---
+++ src/components/chat/ChatBubble.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- <div :class="{ flex: true, 'justify-end': isUserMessage }">
+    <div
+      :class="[
+        isUserMessage ? 'bg-blue-200' : 'bg-gray-300',
+        'text-black',
+        'p-2',
+        'rounded-lg',
+        'max-w-xs',
+      ]"
+    >
+      {{ message }}
+    </div>
+  </div> -->
+
+  <!-- Message user -->
+  <div class="flex justify-end" v-if="isUserMessage">
+    <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
+      {{ message }}
+    </div>
+  </div>
+
+  <!-- recivied user -->
+  <div class="flex" v-else>
+    <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
+      <span class="capitalize">{{ message }}</span>
+      <img v-if="image" :src="image" alt="YesNoImage" class="w-52 h-52 object-cover rounded-md" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  message: string;
+  isUserMessage: boolean;
+  image?: string;
+}
+defineProps<Props>();
+</script>
---
+++ src/components/chat/ChatMessages.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
+      <!-- Messages go here -->
+      <!-- Example Message -->
+      <ChatBubble :is-user-message="true" :message="'Holis'" />
+      <ChatBubble :is-user-message="false" :message="'Saludos'" />
+      <ChatBubble
+        :is-user-message="false"
+        message="No"
+        image="https://yesno.wtf/assets/no/26-34b31d1f0777f70c61488f67a36576a9.gif"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ChatBubble from '@/components/chat/ChatBubble.vue';
+</script>
